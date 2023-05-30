c7c0798a92a16c78f64db1d52d357f3704059008
Darren <darrenslion@gmail.com>
2023-05-30 11:09:03 -0700

    update homepage path

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -123,7 +123,7 @@ const Sidebar = () => {
 
           <Box paddingLeft={isCollapsed ? undefined : "10%"}>
             <Item
-              title="Dashboard"
+              title="/"
               to="/"
               icon={<HomeOutlinedIcon />}
               selected={selected}
